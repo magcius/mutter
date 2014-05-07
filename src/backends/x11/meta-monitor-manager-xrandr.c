65db043f0559977b3fae73fecc3dc9bf53f5080e
Cosimo Cecchi <cosimo@endlessm.com>
2014-05-06 18:24:42 -0700

    MonitorManager: fix compile warnings

--- src/backends/x11/meta-monitor-manager-xrandr.c
+++ src/backends/x11/meta-monitor-manager-xrandr.c
@@ -1313,9 +1313,7 @@ meta_monitor_manager_xrandr_handle_xevent (MetaMonitorManagerXrandr *manager_xra
   MetaMonitorMode *old_modes;
   unsigned int n_old_outputs, n_old_modes;
   gboolean new_config;
-  unsigned i, j, k;
-  XRRScreenChangeNotifyEvent *scevent;
-  Screen *screen;
+  unsigned i, j;
   gboolean needs_update = FALSE;
 
   if ((event->type - manager_xrandr->rr_event_base) != RRScreenChangeNotify)
@@ -1336,8 +1334,8 @@ meta_monitor_manager_xrandr_handle_xevent (MetaMonitorManagerXrandr *manager_xra
   for (i = 0; i < (unsigned)manager->n_outputs; i++)
     {
       MetaOutput *output = &manager->outputs[i];
-      unsigned current_width, current_height;
-      unsigned target_width, target_height;
+      int current_width, current_height;
+      int target_width, target_height;
 
       current_width = output->crtc->current_mode->width;
       current_height = output->crtc->current_mode->height;
@@ -1362,7 +1360,6 @@ meta_monitor_manager_xrandr_handle_xevent (MetaMonitorManagerXrandr *manager_xra
               (current_width != mode->width ||
                current_height != mode->height))
             {
-              Screen *screen;
               int width_mm, height_mm;
               Status ok;
 
@@ -1401,7 +1398,7 @@ meta_monitor_manager_xrandr_handle_xevent (MetaMonitorManagerXrandr *manager_xra
                                      output->crtc->rect.x, output->crtc->rect.y,
                                      (XID)mode->mode_id,
                                      meta_monitor_transform_to_xrandr (output->crtc->transform),
-                                     &output->winsys_id, 1);
+                                     (RROutput *)&output->winsys_id, 1);
 
               XUngrabServer (manager_xrandr->xdisplay);
 
